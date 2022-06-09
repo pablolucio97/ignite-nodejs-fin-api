0b3d2837a26cb72ba2589dc2423f9606c19d9d84
Pablo <pablolucio_@hotmail.com>
2022-06-09 14:51:33 -0300

    Created function to create an account

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const { v4: uuidv4} = require('uuid')
 
 const app = express()
 
-const account = ['one', 'two', 'three', 'four', 'five']
+app.use(express.json())
+
+const customers = []
+
+//CREATE AN ACCOUNT
+
+app.post('/account', (req, res) => {
+    const {cpf, name} = req.body
+    const id = uuidv4()
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return res.status(201).send()
+})
 
 app.get('/account', (req, res) => {
     return res.json(account)
